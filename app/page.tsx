6d87ca1d5252458a6961e32d56570d87faf096be
RobinLarsson1 <robin.larsson01@gmail.com>
2024-09-02 21:55:52 +0200

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,45 @@
 import Image from "next/image";
+import HeroImg from "../public/img/pexels-flodahm-2420412.jpg"
+import Link from "next/link";
+import { Mail } from 'lucide-react';
+import { Phone } from "lucide-react";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <section className="relative w-full h-screen">
+      {/* Image as background */}
+      <div className="absolute inset-0 z-0">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={HeroImg}
+          alt="landing-image"
+          layout="fill" // Ensures the image covers the entire section
+          objectFit="cover" // Ensures the image maintains its aspect ratio while covering the section
+          quality={100} // Optional: to ensure the image quality is high
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Text overlay */}
+      <div className="relative gap-8 z-10 flex items-center h-full flex-col justify-center ">
+        <div className="flex flex-col items-center">
+     <h1 className="lg:text-[84px] leading-[64px] text-[64px] lg:leading-[84px] antialiased font-semibold uppercase">Navolt</h1>
+     <h3 className="text-[18px] antialiased uppercase">Marinelektronik</h3>
+     </div>
+      <p className="w-3/4 antialiased lg:text-[16px] text-[14px] text-center">Vi hjälper dig med felsökning, uppgradering och nyinstallation av elektronik i din fritidsbåt, husbil eller campervan.
+        Allt som rör båtens elsystem tas om hand, inklusive installation av plotter, AIS, autopilot, batteriövervakning, batteriladdare, solpaneler och regulatorer.
+        Vi utför även installationer av litiumbatterier och ser till att ditt elsystem är optimerat och anpassat för att möta dina behov.
+        Låt oss hjälpa dig att ta din utrustning till nästa nivå – kontakta oss idag för en lösning som är skräddarsydd för just dig!
+        Baserade på Öckerö i Göteborg, täcker vi större delen av västkusten med våra tjänster.
+        Hör av dig idag för att få hjälp med din el ombord!
+        </p>
+      <div>
+<h4 className="uppercase text-white text-center font-semibold mb-4 mt-10 text-[24px] z-10">Kontakt</h4>
+<div className="flex flex-col items-center gap-2">
+<p className="flex items-center gap-2"> <Phone />073 025 45 55</p>
+<Link href="mailto:jacob@navolt.se" className="flex items-center gap-2"> <Mail height={30} width={30} stroke="white"/>Jacob@navolt.se</Link>
+</div>
       </div>
-    </main>
+      </div>
+    </section>
   );
 }
