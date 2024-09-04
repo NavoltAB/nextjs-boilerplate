2d23abae5a94f0eb5ff241890c4f2a49eb876ae7
Robin Larsson <Robin.Larsson01@gmail.com>
2024-09-04 19:58:49 +0200

    Fix: korr info-text

--- app/page.tsx
+++ app/page.tsx
@@ -22,22 +22,24 @@ export default function Home() {
       {/* Text overlay */}
       <div className="relative gap-8 z-10 flex items-center h-full flex-col justify-center ">
         <div className="flex flex-col items-center">
-     <h1 className="lg:text-[84px] leading-[64px] text-[64px] lg:leading-[84px] antialiased font-semibold uppercase">Navolt</h1>
-     <h3 className="text-[18px] antialiased uppercase">Marinelektronik</h3>
+     <h1 className="lg:text-[84px] leading-[64px] text-white text-[64px] lg:leading-[84px] antialiased font-semibold uppercase">Navolt</h1>
+     <h3 className="text-[18px] antialiased text-white uppercase">Marinelektronik</h3>
      </div>
-      <p className="w-3/4 antialiased lg:text-[16px] text-[14px] text-center">Vi hjälper dig med felsökning, uppgradering och nyinstallation av elektronik i din fritidsbåt, husbil eller campervan.
-        Allt som rör båtens elsystem tas om hand, inklusive installation av plotter, AIS, autopilot, batteriövervakning, batteriladdare, solpaneler och regulatorer.
-        Vi utför även installationer av litiumbatterier och ser till att ditt elsystem är optimerat och anpassat för att möta dina behov.
-        Låt oss hjälpa dig att ta din utrustning till nästa nivå – kontakta oss idag för en lösning som är skräddarsydd för just dig!
-        Baserade på Öckerö i Göteborg, täcker vi större delen av västkusten med våra tjänster.
-        Hör av dig idag för att få hjälp med din el ombord!
+      <p className="w-3/4 antialiased lg:text-[16px] text-[14px] text-center">Vi hjälper dig med felsökning, uppgradering och nyinstallation av elektronik i din fritidsbåt, husbil eller campervan. 
         </p>
       <div>
-<h4 className="uppercase text-white text-center font-semibold mb-4 mt-10 text-[24px] z-10">Kontakt</h4>
+<h4 className="uppercase text-white text-center font-semibold mb-2 mt-10 text-[24px] z-10">Kontakt</h4>
 <div className="flex flex-col items-center gap-2">
 <p className="flex items-center gap-2"> <Phone />073 025 45 55</p>
-<Link href="mailto:jacob@navolt.se" className="flex items-center gap-2"> <Mail height={30} width={30} stroke="white"/>Jacob@navolt.se</Link>
+<Link href="mailto:info@navolt.se" className="flex items-center gap-2"> <Mail height={30} width={30} stroke="white"/>info@navolt.se</Link>
 </div>
+      </div>
+      <div className="[&>p]:text-xs text-white/70 absolute bottom-3 right-3 [&>p]:antialiased ">
+        <p>Navolt AB</p>
+        <p>Org. nummer: 559475-1876</p>
+        <p>Postadress: Kapellevägen 15B</p>
+        <p>451 44 Uddevalla</p>
+        <p>Bolaget är godkänt för F-skatt</p>
       </div>
       </div>
     </section>
