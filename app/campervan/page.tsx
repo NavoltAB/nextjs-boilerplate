07d983a40a853c03a4b060f29839dce306c36b60
Robin Larsson <Robin.Larsson01@gmail.com>
2024-09-09 21:34:24 +0200

    fix: unloaded images

--- app/campervan/page.tsx
+++ app/campervan/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Campervan() {
   return (
-    <section className="min-h-[calc(100vh-70px)] flex flex-col items-center justify-center md:min-h-[calc(100vh-99px)]">
+    <section className="min-h-[calc(100vh-70px)] flex flex-col items-center bg-black justify-center md:min-h-[calc(100vh-99px)]">
       <div className="absolute inset-0 z-0">
         <Image
           src={CamperImg}
