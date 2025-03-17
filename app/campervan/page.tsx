c350402b2b0a0bc44be44980e81991b57234179b
RobinLarsson1 <Robin.Larsson01@gmail.com>
2025-03-17 15:17:17 +0100

    fix: z-index

--- app/campervan/page.tsx
+++ app/campervan/page.tsx
@@ -46,12 +46,12 @@ export default function Campervan() {
     {
       heading: "Kopplingsschema",
       text: "Vi skapar ett detaljerat kopplingsschema anpassat specifikt för dina komponenter.",
-      icon: <Cable className="w-8 h-8" strokeWidth={1.25}/>
+      icon: <Cable className="w-8 h-8" strokeWidth={1.25} />,
     },
     {
       heading: "Systemdesign",
       text: "Vi hjälper dig att designa ditt elsystem från grunden, anpassat efter dina förutsättningar, behov och önskemål.",
-      icon: <Wrench className="w-8 h-8" strokeWidth={1.25}/>
+      icon: <Wrench className="w-8 h-8" strokeWidth={1.25} />,
     },
     {
       heading: "Konsultation",
@@ -94,7 +94,7 @@ export default function Campervan() {
         role="img"
         aria-label="landing-image"
       />
-      <div className="relative w-full ] flex-col items-center py-6 gap-4 flex justify-center z-60">
+      <div className="relative w-full ] flex-col items-center py-6 gap-4 flex justify-center z-40">
         <h1 className="text-white text-center  lg:text-[32px] px-10 text-[24px]  font-semibold uppercase">
           {" "}
           Bygg ditt elsystem själv - eller låt oss göra det åt dig!{" "}
@@ -135,15 +135,23 @@ export default function Campervan() {
         <div className="relative flex h-fit-content items-center justify-center z-[70] mt-4">
           <Dialog>
             <DialogTrigger asChild>
-              <Button className="w-fit lg:text-md text-sm hover:bg-white hover:scale-[1.02] bg-white rounded-full text-black uppercase font-bold md:py-6 py-2 px-6 md:px-8">Berätta om din van</Button>
+              <Button className="w-fit lg:text-md text-sm hover:bg-white hover:scale-[1.02] bg-white rounded-full text-black uppercase font-bold md:py-6 py-2 px-6 md:px-8">
+                Berätta om din van
+              </Button>
             </DialogTrigger>
             <DialogContent className="p-0 bg-[#111111] max-h-[90vh] overflow-y-auto border-none !rounded-none z-[80]">
               <div className="h-full w-full">
                 <DialogTitle hidden>
                   <h3>Berätta om din van</h3>
                 </DialogTitle>
-                <script src="https://static.elfsight.com/platform/platform.js" async></script>
-                <div className="elfsight-app-4daba287-9bb2-4f97-8c90-9b215ae72512" data-elfsight-app-lazy></div>
+                <script
+                  src="https://static.elfsight.com/platform/platform.js"
+                  async
+                ></script>
+                <div
+                  className="elfsight-app-4daba287-9bb2-4f97-8c90-9b215ae72512"
+                  data-elfsight-app-lazy
+                ></div>
               </div>
             </DialogContent>
           </Dialog>
